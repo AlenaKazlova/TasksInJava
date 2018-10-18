06d45605e7beff06be1e1d7823af399d8d426ae8
AlenaKazlova <44174098+AlenaKazlova@users.noreply.github.com>
2018-10-19 01:25:36 +0300

    Update LogicWeightDinosaur.java

--- src/com/kazlova/task01/LogicWeightDinosaur.java
+++ src/com/kazlova/task01/LogicWeightDinosaur.java
@@ -6,7 +6,7 @@ public class LogicWeightDinosaur {
 	static final double KG_TO_Mg = 1000000;
 	static final double KG_TO_T = 0.001;
 	
-	public static double calcWeightG(double weightKg) {
+    public static double calcWeightG(double weightKg) {
     	return weightKg*KG_TO_G;
     }
     public static double calcWeightMg(double weightKg) {
